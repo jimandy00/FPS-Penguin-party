3e151f7fc6e7f96c5ebd97aea98fc7ab8c3c79cd
Jihwan Byeon <wkdghk24@gmail.com>
2023-07-28 11:03:25 +0900

    [feat] Timer
    Timer 구현 완료

--- Assets/Scripts/TimeManager.cs
+++ Assets/Scripts/TimeManager.cs
@@ -11,36 +11,56 @@ public class TimeManager : MonoBehaviour
     // 남은 시간 표시
     public Text timeTextUI;
 
+    // 현재 시간
+    float currentTime;
+
     // 제한시간
     float limitTime = 120f;
 
     int min, sec;
 
+    private void Timer()
+    {
+        if (currentTime > 0)
+        {
+            currentTime -= Time.deltaTime;
+            min = (int)currentTime / 60;
+            sec = (int)currentTime % 60;
+            timeTextUI.text = $"{min} : {sec}";
+            Debug.Log("시간 가는중");
+        }
+        else if (currentTime == 0)
+        {
+            Stop_Timer();
+        }
+    }
+
+    private void Stop_Timer()
+    {
+        timeTextUI.text = "00 : 00";
+        Debug.Log("타이머 멈춤");
+    }
+
+    private void Reset_Timer()
+    {
+        currentTime = limitTime;
+        min = (int)currentTime / 60;
+        sec = (int)currentTime % 60;
+        timeTextUI.text = $"{min} : {sec}";
+        Debug.Log("시간 초기화");
+    }
+
+
+
     // Start is called before the first frame update
     void Start()
     {
-        timeTextUI.text =  "02 : 00";
+        Reset_Timer();
     }
 
     // Update is called once per frame
     void Update()
     {
-        while(limitTime > 0)
-        {
-            // 시간이 지나면서 제한시간 -1초씩 감소
-           limitTime -= (int)Time.deltaTime;
-
-            if(limitTime < 60)
-            {
-                timeTextUI.text = "00 : " + sec;
-            }
-
-            min = (int)limitTime / 60;
-            sec = (int)limitTime % 60;
-
-            timeTextUI.text = min + " : " + sec;
-        }
-
-        timeTextUI.text = "00 : 00";
+        Timer();
     }
 }
